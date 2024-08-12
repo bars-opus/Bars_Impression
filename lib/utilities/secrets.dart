8f1e75be3f4cead41630a5eb99cd596b4e7f9fb7
bars-opus <109417067+bars-opus@users.noreply.github.com>
2024-08-12 23:39:57 +0000

    hide_api keys

---
+++ lib/utilities/secrets.dart
@@ -0,0 +1,18 @@
+class GeminiApiKey {
+  static const String GEMINIAPI_KEY = 'AIzaSyBz9rqHwd___DFD7YgD0J26XoMpcOaX_KY';
+}
+
+class GooglemapApiKey {
+  static const String GOOGLEMAPAPI_KEY =
+      'AIzaSyBzHyTS-J9Ge8ohh8A1fZANeLqQbGHcGQY';
+}
+
+class visionHelperApiKey {
+  static const String VISIONHELPERPAPI_KEY =
+      'AIzaSyBSKY9WsjiLuCprwn9Ay-VaE226lbtJAfw';
+}
+
+class PayStackKey {
+  static const String PAYSTACK_KEY =
+      'pk_live_da89bb63e3be738c9c8349da5297d71e871a3596';
+}
